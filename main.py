37069c1868eda263960c7c92e9f25fb4602ea313
hdariodev <hdariodev@gmail.com>
2024-06-14 21:16:56 -0400

    feat: add GET /movies endpoint

--- main.py
+++ main.py
@@ -5,6 +5,29 @@ app.title = "My API"
 app.description = "This is a fantastic API"
 app.version = "1.0.0"
 
+movies = [
+    {
+        "id": 1,
+        "title": "The Shawshank Redemption",
+        "overview": "lorem ipsum dolor sit amet ... ",
+        "year": 1994,
+        "rating": 9.3,
+        "category": "Drama"
+    },
+    {
+        "id": 2,
+        "title": "The Godfather",
+        "overview": "lorem ipsum dolor sit amet ... ",
+        "year": 1972,
+        "rating": 9.2,
+        "category": "Crime"
+    },
+]
+
 @app.get("/", tags=["Root"])
 def read_root():
     return {"Hello": "World"}
+
+@app.get('/movies', tags=['Movies'])
+def read_movies():
+    return movies
