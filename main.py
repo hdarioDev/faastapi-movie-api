e1935c2bf6558c3579badd245ff687ea0686d2ae
hdariodev <hdariodev@gmail.com>
2024-06-14 20:46:23 -0400

    feat: custom descriptions for Swagger

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+app.title = "My API"
+app.description = "This is a fantastic API"
+app.version = "1.0.0"
 
-@app.get("/")
+@app.get("/", tags=["Root"])
 def read_root():
     return {"Hello": "World"}
